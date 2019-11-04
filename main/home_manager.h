ab3659b7e7c91a62d189db92d19f3000700cefe5
a.liut <andrea.liut@gmail.com>
2019-11-04 23:54:01 +0100

    Updated endpoints.

--- main/home_manager.h
+++ main/home_manager.h
@@ -1,8 +1,8 @@
 #ifndef H_HOME_MANAGER
 #define H_HOME_MANAGER
 
-#define HOME_MANAGER_REGISTRATION_URL "http://192.168.1.198:8080/devices"
-#define HOME_MANAGER_DATA_URL "http://192.168.1.198:8080/devices/"
+#define HOME_MANAGER_REGISTRATION_URL "http://192.168.1.123:8080/devices"
+#define HOME_MANAGER_DATA_URL "http://192.168.1.123:8080/devices/data/"
 
 bool isRegistered();
 int registerDevice(const char* id, int idSize);
